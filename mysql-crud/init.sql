c36c5f433fea4706f18adf53418b842062ac21ef
박종우 <indralight79@gmail.com>
2022-08-15 20:02:31 +0900

    sql

---
+++ mysql-crud/init.sql
@@ -0,0 +1,33 @@
+CREATE SCHEMA `nodejs` DEFAULT CHARACTER SET utf8;
+
+use nodejs;
+
+CREATE TABLE nodejs.users (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL,
+    age INT UNSIGNED NOT NULL,
+    married TINYINT NOT NULL,
+    comment TEXT NULL,
+    created_at DATETIME NOT NULL DEFAULT now(),
+    PRIMARY KEY(id),
+    UNIQUE INDEX name_UNIQUE (name ASC))
+    COMMENT = '사용자 정보'
+    DEFAULT CHARACTER SET = utf8
+    ENGINE = InnoDB;
+
+CREATE TABLE nodejs.comments (
+    id INT NOT NULL AUTO_INCREMENT,
+    commenter INT NOT NULL,
+    comment VARCHAR(100) NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT now(),
+    PRIMARY KEY(id),
+    INDEX commenter_idx (commenter ASC),
+    CONSTRAINT commenter
+    FOREIGN KEY (commenter)
+    REFERENCES nodejs.users (id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+    COMMENT = '댓글'
+    DEFAULT CHARACTER SET = utf8
+    ENGINE = InnoDB;
+

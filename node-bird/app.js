dda6ff8c59584af9de70531237e9535bb560210e
박종우 <indralight79@gmail.com>
2022-08-16 21:23:10 +0900

    nodebird

--- node-bird/app.js
+++ node-bird/app.js
@@ -1,3 +1,24 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const pageRouter = require('./routes/page');
+
+const app = express();
+app.set('port', process.env.PORT || 8001);
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+  express: app,
+  watch: true,
+});
+
+app.use(morgan('dev'));
+
 const { sequelize } = require('./models');
 
 sequelize

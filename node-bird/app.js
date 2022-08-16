d8087e2f7b6c594f343521811fdf0fe90a632816
박종우 <indralight79@gmail.com>
2022-08-16 18:07:23 +0900

    nodebird

---
+++ node-bird/app.js
@@ -0,0 +1,10 @@
+const { sequelize } = require('./models');
+
+sequelize
+  .sync({ force: false })
+  .then((result) => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
